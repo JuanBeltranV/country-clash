fc9e1d89128772ff96b04e45b0fdcbf8243feef2
JuanBeltranV <jua.beltranv@duocuc.cl>
2025-11-01 23:06:01 -0300

    Correccion deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/country-clash/',   // el nombre exacto del repo
   plugins: [react()],
 })
